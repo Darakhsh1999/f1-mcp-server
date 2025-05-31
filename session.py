be418ff4f36b0c1b27b450dbaedcd9da90f31710
Darakhsh1999 <darakrash@protonmail.com>
2025-05-31 09:49:49 +0200

    foundational modules

---
+++ session.py
@@ -0,0 +1,20 @@
+import fastf1
+import datetime
+
+class F1Session():
+
+    def __init__(self, year=None, ):
+
+        self.year = datetime.datetime.now().year if year is None else year
+        
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
